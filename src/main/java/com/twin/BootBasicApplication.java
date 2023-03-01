c7333c98a8a8159be98c5d347f7cc38e611bd582
jiefury <296553550@qq.com>
2023-03-01 18:24:11 +0800

    1231

--- src/main/java/com/twin/BootBasicApplication.java
+++ src/main/java/com/twin/BootBasicApplication.java
@@ -3,6 +3,7 @@ package com.twin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -12,6 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableSwagger2
+@EnableDiscoveryClient
 public class BootBasicApplication {
 
     public static void main(String[] args) {
