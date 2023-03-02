f87d9c4d3cdb19601a82f7e36f52a6292e808636
yaofusheng <jiefury@163.com>
2023-03-02 21:55:13 +0800

    56

--- src/main/java/com/twin/BootBasicApplication.java
+++ src/main/java/com/twin/BootBasicApplication.java
@@ -1,10 +1,16 @@
 package com.twin;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.scheduling.annotation.EnableAsync;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author itranlin
@@ -12,10 +18,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableSwagger2
+@EnableDiscoveryClient
 public class BootBasicApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(BootBasicApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(BootBasicApplication.class, args);
+        while(true){
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            String username = environment.getProperty("user.name");
+            String age = environment.getProperty("user.age");
+            System.out.println("username:"+username+" | age:"+age);
+            TimeUnit.SECONDS.sleep(1);
+        }
     }
 
 }
